ff2b09e727c24b982dd236fec5053344ec99109b
Tom de Bruin <tom@deadlyhifi.com>
2015-08-10 21:28:36 +0100

    The wp_title filter

--- THEME_NAME/functions/customisations-theme.php
+++ THEME_NAME/functions/customisations-theme.php
@@ -7,7 +7,41 @@ class WpThemeCustomisations
 {
     public function __construct()
     {
+        add_filter('wp_title', [$this, 'wp_title'], 10, 2);
         add_filter('pre_get_posts', [$this, 'inlineStickyPosts']);
+    }
+    
+        /**
+     * Filters wp_title to print a neat <title> tag based on what is being viewed.
+     *
+     * @param string $title Default title text for current view.
+     * @param string $sep Optional separator.
+     * @return string The filtered title.
+     */
+    public function wp_title($title, $sep)
+    {
+        if (is_feed()) {
+            return $title;
+        }
+
+        global $page, $paged;
+
+        // Add the blog name.
+        $title .= get_bloginfo('name', 'display');
+
+        // Add the blog description for the home/front page.
+        $site_description = get_bloginfo('description', 'display');
+
+        if ($site_description && (is_home() || is_front_page())) {
+            $title .= " $sep $site_description";
+        }
+
+        // Add a page number if necessary.
+        if (($paged >= 2 || $page >= 2) && ! is_404()) {
+            $title .= " $sep " . sprintf(esc_html__('Page %s', '_s'), max($paged, $page));
+        }
+
+        return $title;
     }
 
     /**
