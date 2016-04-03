f067ffb79767c449ccfa2da055fc8b1969ade998
Matthew Kimont <matthew.kimont@wearemarvellous.com>
2016-04-03 14:37:32 +0100

    remove emoji script

--- THEME_NAME/functions.php
+++ THEME_NAME/functions.php
@@ -55,6 +55,21 @@ if (! function_exists('THEME_NAME_setup')) :
             'caption'
         ]);
 
+        function disable_wp_emojicons() {
+          // all actions related to emojis
+          remove_action( 'admin_print_styles', 'print_emoji_styles' );
+          remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+          remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+          remove_action( 'wp_print_styles', 'print_emoji_styles' );
+          remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+          remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+          remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+          // filter to remove TinyMCE emojis
+          add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+        }
+        add_action( 'init', 'disable_wp_emojicons' );
+
         /*
          * Enable support for Post Formats.
          * See http://codex.wordpress.org/Post_Formats
